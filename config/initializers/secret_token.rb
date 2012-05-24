3b8204b80cf84d16eaf2db1a473f6245084e122e
Dominik Grygiel <dominik.grygiel@gmail.com>
2012-05-24 16:51:25 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copywriter::Application.config.secret_token = 'd419b7305e10dc79aa099ca6766806c4c7a0aa80fe52bc05e6797c3beeb62ee27f8929442fa5e9f82a16071c74bb9bf2a2c71a8ee3c1556e1f76f4b3e7f4155e'
